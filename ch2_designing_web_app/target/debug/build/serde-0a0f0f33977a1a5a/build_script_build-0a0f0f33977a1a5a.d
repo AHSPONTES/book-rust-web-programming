67d01ae8865f313294486bcf370d72c2fd0c3292
AHSPONTES <65411128+AHSPONTES@users.noreply.github.com>
2021-07-21 11:33:38 -0300

    Delete ch2_designing_web_app/target directory

--- ch2_designing_web_app/target/debug/build/serde-0a0f0f33977a1a5a/build_script_build-0a0f0f33977a1a5a.d
+++
@@ -1,5 +0,0 @@
-/home/andre/Projetos/book_RustWebProgramming/book-rust-web-programming/ch2_designing_web_app/target/debug/build/serde-0a0f0f33977a1a5a/build_script_build-0a0f0f33977a1a5a: /home/andre/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.126/build.rs
-
-/home/andre/Projetos/book_RustWebProgramming/book-rust-web-programming/ch2_designing_web_app/target/debug/build/serde-0a0f0f33977a1a5a/build_script_build-0a0f0f33977a1a5a.d: /home/andre/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.126/build.rs
-
-/home/andre/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.126/build.rs:
