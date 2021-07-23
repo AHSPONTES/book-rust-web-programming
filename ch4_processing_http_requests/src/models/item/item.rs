12cd95d7635d2a0f42144ecb65098877f97b3325
AndreHSP <andrehspontes@gmail.com>
2021-07-23 10:16:58 -0300

    getting data from the database

--- ch4_processing_http_requests/src/models/item/item.rs
+++ ch4_processing_http_requests/src/models/item/item.rs
@@ -3,7 +3,7 @@ use crate::schema::to_do;
 #[derive(Queryable, Identifiable)]
 #[table_name = "to_do"]
 pub struct Item {
-    pub id: u32,
+    pub id: i32,
     pub title: String,
     pub status: String,
 }
