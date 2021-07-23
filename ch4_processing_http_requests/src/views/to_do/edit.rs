fe62dfd63458811f5f5f38e74abaf13b8ce100c9
AndreHSP <andrehspontes@gmail.com>
2021-07-23 12:43:56 -0300

    editing the database

--- ch4_processing_http_requests/src/views/to_do/edit.rs
+++ ch4_processing_http_requests/src/views/to_do/edit.rs
@@ -1,40 +1,23 @@
+use crate::diesel;
+use diesel::prelude::*;
+
 use actix_web::{web, HttpResponse};
-use serde_json::{value::Value, Map};
 
 use super::utils::return_state;
-use crate::state::read_file;
 
+use crate::database::establish_connection;
 use crate::json_serialization::to_do_item::ToDoItem;
-use crate::processes::process_input;
-use crate::to_do::to_do_factory;
+use crate::schema::to_do;
 
 pub async fn edit(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json");
-
     let title: String = to_do_item.title.clone();
-    let title_reference: &String = &title.clone();
 
-    let status: String;
-    match &state.get(title_reference) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            return HttpResponse::NotFound().json(format!("{} not in state", title_reference));
-        }
-    }
-    if &status == &to_do_item.status {
-        /*  */
-        return HttpResponse::Ok().finish();
-    }
+    let connection = establish_connection();
+    let results = to_do::table.filter(to_do::columns::title.eq(title));
 
-    match to_do_factory(status.as_str(), title.as_str()) {
-        Err(_item) => {
-            HttpResponse::BadRequest().json(format!("{} not accepted", status));
-        }
-        Ok(item) => {
-            process_input(item, String::from("edit"), &state);
-        }
-    }
+    let _ = diesel::update(results)
+        .set(to_do::columns::status.eq("done"))
+        .execute(&connection);
+
     HttpResponse::Ok().json(return_state())
 }
