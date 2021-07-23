2cfcacb4bba61089d389988186ebd51699cff2d6
AndreHSP <andrehspontes@gmail.com>
2021-07-23 15:16:40 -0300

    altering the to-do item data model

--- ch4_processing_http_requests/src/models/item/new_item.rs
+++ ch4_processing_http_requests/src/models/item/new_item.rs
@@ -5,12 +5,14 @@ use crate::schema::to_do;
 pub struct NewItem {
     pub title: String,
     pub status: String,
+    pub user_id: i32,
 }
 impl NewItem {
     pub fn new(title: String) -> NewItem {
         NewItem {
             title,
             status: String::from("pending"),
+            user_id,
         }
     }
 }
